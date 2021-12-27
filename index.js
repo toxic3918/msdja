3a1dbbd17524acb928c2b99b5f1221e26aceff81
toxic3918 <89975312+toxic3918@users.noreply.github.com>
2021-12-27 12:46:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client, Collection } = require("discord.js");
+
+const client = new Client({
+    intents: 32767,
+});
+module.exports = client;
+
+// Global Variables
+client.commands = new Collection();
+client.slashCommands = new Collection();
+client.config = require("./config.json");
+
+client.on('message', message => {
+    if (message.content === '+ping') {  
+      message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+    }
+  });
+client.login(client.config.token); 
